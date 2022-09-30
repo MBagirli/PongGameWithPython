90f0ecf1fb37642bbfb50ef2a74e77e1ec815f5d
MBagirli <104429881+MBagirli@users.noreply.github.com>
2022-09-30 22:58:32 +0400

    Add files via upload

---
+++ 22 day/paddle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super(Paddle, self).__init__()
+        self.penup()
+        self.shape("square")
+        self.turtlesize(stretch_wid=5,stretch_len=1)
+        self.color("white")
+        self.goto(position[0],position[1])
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.sety(new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.sety(new_y)
